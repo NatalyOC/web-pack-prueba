41847a505f3dfa859c0c1bccab8e0cc995710af3
NatalyOC <noteroc@hotmail.com>
2018-03-13 11:39:43 -0500

    first commit

---
+++ webpack.config.prod.js
@@ -0,0 +1,7 @@
+const { resolve } = require('path');
+
+// Importamos webpack
+const webpack = require('webpack');
+// Importamos los plugins que habiamos instalado y los declaramos
+const OpenBrowserPlugin = require('open-browser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
